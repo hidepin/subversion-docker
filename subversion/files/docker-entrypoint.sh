367d6743b94b8e5acde2b246b0eee1251c5ff724
Hidetoshi Imai <hidepin@gmail.com>
2016-09-15 00:06:08 +0900

    add initial dir setting.

--- subversion/files/docker-entrypoint.sh
+++ subversion/files/docker-entrypoint.sh
@@ -4,9 +4,14 @@ set -e
 
 for REPO in $(echo -n ${REPOS} | sed 's/:/ /g')
 do
-    if [ ! -f "/opt/svn/${REPO}repo/README.txt" ] && [ `cat "/opt/svn/${REPO}repo/README.txt" | grep "This is a Subversion repository" | wc -l` -ne 1 ]; then
+    if [ ! -f "/opt/svn/${REPO}repo/README.txt" ] || [ `cat "/opt/svn/${REPO}repo/README.txt" | grep "This is a Subversion repository" | wc -l` -ne 1 ]; then
         echo "generate ${REPO}repo."
         svnadmin create --fs-type fsfs /opt/svn/${REPO}repo
+        svn mkdir --parents file:///opt/svn/${REPO}repo/${REPO}doc/trunk \
+                            file:///opt/svn/${REPO}repo/${REPO}app/trunk \
+                            file:///opt/svn/${REPO}repo/${REPO}app/branches \
+                            file:///opt/svn/${REPO}repo/${REPO}app/tags \
+                            -m "initial create dir hier."
         chown -R apache:apache /opt/svn
     fi
 done
